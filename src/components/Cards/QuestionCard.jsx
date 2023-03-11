80d95418c677b0074856ff44df9072c38d200d5f
michael07200 <mickmhtong@gmail.com>
2023-03-11 11:46:55 +0100

    build: timer page

---
+++ src/components/Cards/QuestionCard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { useRouter } from "next/router";
+import QuestionGroups from "../../data/QuestionGroup.json";
+
+const QuestionCard = () => {
+  const router = useRouter();
+  const questionId = router.query.id;
+
+  let questionData = null;
+  // Loop through the values of QuestionGroups and search for the question data object that matches the ID
+  for (const questions of Object.values(QuestionGroups)) {
+    questionData = questions.find((item) => item.id === Number(questionId));
+
+    if (questionData) {
+      break;
+    }
+  }
+
+  if (!questionData) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <h1>
+        <a href={questionData.url}>{questionData.title} Timer</a>
+      </h1>
+      <p>Duration: {questionData.duration} minutes</p>
+      <p>Difficulty: {questionData.difficulty}</p>
+      <p>Topic: {questionData.topic}</p>
+      <p>Routines: {questionData.routines.join(", ")}</p>
+    </div>
+  );
+};
+
+export default QuestionCard;
---
+++ src/pages/question/[id]/timer.jsx
@@ -0,0 +1,13 @@
+import QuestionCard from "@/components/Cards/QuestionCard";
+import React from "react";
+
+const timer = () => {
+  return (
+    <div>
+      <h1>timer</h1>
+      <QuestionCard />
+    </div>
+  );
+};
+
+export default timer;
