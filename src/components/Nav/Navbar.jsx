8d049261fef0ecb8a815833cde23f122cb387ffc
michael07200 <mickmhtong@gmail.com>
2023-03-22 20:29:59 +0100

    build: nav icon

--- src/components/Nav/Navbar.jsx
+++ src/components/Nav/Navbar.jsx
@@ -1,15 +1,14 @@
 import { useState } from "react";
 import { Dialog } from "@headlessui/react";
 import Link from "next/link";
-import DropdownLink from "../Button/DropdownLink";
-import Toggler from "./Toggler";
 import { HiBars3, HiXMark } from "react-icons/hi2";
 import GoogleAuth from "../Auth/GoogleAuth";
+import Image from "next/image";
 
 const navigation = [
   { name: "Home", href: "/" },
-  { name: "Progress", href: "/progress" },
   { name: "Suggestion", href: "/suggestion" },
+  { name: "My Progress", href: "/progress" },
 ];
 
 const Navbar = () => {
@@ -18,15 +17,11 @@ const Navbar = () => {
   return (
     <>
       <header className="absolute inset-x-0 top-0 z-50">
-        <nav className="flex items-center justify-between p-6 lg:px-8" aria-label="Global">
+        <nav className="flex items-center justify-between p-6 lg:px-36 xl:px-60" aria-label="Global">
           <div className="flex lg:flex-1">
             <Link href="/" className="-m-1.5 p-1.5">
-              <span className="sr-only">Your Company</span>
-              <img
-                className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                alt=""
-              />
+              <span className="sr-only">Let's code</span>
+              <Image src="/logo.png" width="40" height="40" />
             </Link>
           </div>
           <div className="flex lg:hidden">
@@ -60,12 +55,8 @@ const Navbar = () => {
           <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
             <div className="flex items-center justify-between">
               <a href="#" className="-m-1.5 p-1.5">
-                <span className="sr-only">Your Company</span>
-                <img
-                  className="h-8 w-auto"
-                  src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                  alt=""
-                />
+                <span className="sr-only">Let's code</span>
+                <Image src="/logo.png" width="40" height="40" />
               </a>
               <button
                 type="button"
