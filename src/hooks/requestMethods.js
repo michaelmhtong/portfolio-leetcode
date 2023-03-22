dab8949516ddfdff3efd79a3480090f034223e1b
michael07200 <mickmhtong@gmail.com>
2023-03-22 21:48:15 +0100

    test: fix url error

--- src/hooks/requestMethods.js
+++ src/hooks/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000/api/";
+const BASE_URL = `${process.env.NEXTAUTH_URL}/api/`;
 
 // const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 // const currentUser = user && JSON.parse(user).currentUser;
